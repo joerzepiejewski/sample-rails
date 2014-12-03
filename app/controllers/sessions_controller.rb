b1ba871fe2a59364d61d04263e0831ba4503f890
Joe Rzepiejewski <joe@pearsports.com>
2014-12-02 21:03:23 -0800

    minor tweak to home page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       # Log the user in and redirect to the user's show page.
       flash.clear
       log_in user
-      redirect_to user      
+      redirect_to root_url     
     else
       flash[:danger] = 'Invalid email/password combination' # Not quite right!
       render 'new'
